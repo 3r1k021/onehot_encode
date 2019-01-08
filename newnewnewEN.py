d678548f9760868d8432bcfa1148a6e992adf9b1
3r1k021 <erikossner1@yahoo.com>
2019-01-08 18:19:37 -0500

    Create newnewnewEN.py

---
+++ newnewnewEN.py
@@ -0,0 +1,97 @@
+# cook your code here
+#possible values
+values = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/-! '
+# dictionaries of char->int and int->char
+char_to_int = dict((c, i) for i, c in enumerate(values))
+int_to_char = dict((i, c) for i, c in enumerate(values))
+
+
+def encode(data):
+    # integer encoding (returns numerical representation of char)
+    if type(data) is int:
+        data=str(data)
+    int_encode = [char_to_int[char] for char in data]
+    # one hot encode ()
+    onehot_encode = list()
+    for value in int_encode:
+    	letter = [0 for _ in range(len(values))]
+        letter[value] = 1
+        onehot_encode.append(letter)
+    return onehot_encode
+#End encode
+
+
+def decode(data):
+    index=0;
+    word=''
+    for row in data:
+        index=0;
+        for element in row:
+            if element!=0:
+                word+=(str(values[index]))
+            index+=1
+            
+    return word
+#End encode
+
+def encode_all(training):
+    if type(training) is dict:
+        encode_data={} 
+        for key,val in training.items():
+            if type(val) is dict or type(val) is list:
+                encode_data[key]=encode_all(val)   #recursion to find deepest level in dictionary
+            else:
+                encode_data[key]=encode(val)
+        return encode_data
+    elif type(training) is list:
+        encode_data=[]
+        for val in training:
+            if type(val) is dict or type(val) is list:
+                encode_data.append(encode_all(val))   #recursion to find deepest level in dictionary
+            else:
+                encode_data.append(encode(val))
+        return encode_data
+
+    return 0
+    
+def decode_all(training):
+    if type(training) is dict:
+        decode_data={}
+        for key,val in training.items():
+            if type(val) is dict or type(val) is list:
+                encode_all(val)  
+                break
+            else:
+               return 0
+        return encode_data
+    elif type(training) is list:
+        encode_data=[]
+        for val in training:
+            if type(val) is dict or type(val) is list:
+                encode_data.append(encode_all(val))   #recursion to find deepest level in dictionary
+            else:
+                encode_data.append(encode(val))
+        return encode_data
+
+    return 0 
+    
+
+def pull_data(data, desired):
+    new_list={}
+    if type(data) is dict:
+        for key,val in data.items():
+            if type(val) is dict:
+                to_add=pull_data(val,desired)   #Data type in value is another list or dict, so go another layer deeper
+                for k,v in to_add.items():
+                    new_list[k]=v
+            else:
+                if key in desired:
+                    new_list[key]=val
+    return new_list
+            
+    
+fulldata={ "Qal": "Erik","nons": [3,4,5],"sal": [3,4,{"e": [3,4,5]}],"erikkk": 3,"host": {"t": [3,3,1232]},"qaz": "mew"}
+relevant=pull_data(fulldata,['qaz','t'])
+
+for key,val in relevant.items():
+    print key,val
