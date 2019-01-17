a6e9cf63dae9357569937e1bfedf11978a98b62e
3r1k021 <erikossner1@yahoo.com>
2019-01-17 02:19:11 -0500

    Create learn.py

---
+++ learn.py
@@ -0,0 +1,55 @@
+# Import libraries for data wrangling, preprocessing and visualization
+import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+
+# Read data file
+data = pd.read_csv("../input/data.csv", header=0)
+seed = 5
+numpy.random.seed(seed)
+
+# Column Unnamed : 32 holds only null values, so it is of no use to us. We simply drop that column.
+
+
+# Select the columns to use for prediction in the neural network
+prediction_var = ['hostname', 'cluster', 'parentcluster','building','cloud','spec','manufacturer','dc','model','owners','tags']
+X = data[prediction_var].values
+Y = data.under_utilized.values
+
+# Diagnosis values are strings. Changing them into numerical values using LabelEncoder.
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+
+
+# Baseline model for the neural network. We choose a hidden layer of 10 neurons. The lesser number of neurons helps to eliminate the redundancies in the data and select the more important features.
+def create_baseline():
+    # create model
+    model = Sequential()
+    model.add(Dense(10, input_dim=30, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+    # Compile model. We use the the logarithmic loss function, and the Adam gradient optimizer.
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+# Evaluate model using standardized dataset.
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=5, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True)
+results = cross_val_score(pipeline, X, encoded_Y, cv=kfold)
+print("Results: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
