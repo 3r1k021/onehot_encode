2ca0151fe82cde979b080a421ed1b590e003ca44
3r1k021 <erikossner1@yahoo.com>
2019-03-18 17:55:22 -0400

    Create toolazytofixduringwork.py

---
+++ toolazytofixduringwork.py
@@ -0,0 +1,7 @@
+training_line=np.array([last_full])
+ 97     if int(num_len)>int(training_line.shape[1]):
+ 98
+ 99     elif int(num_len)<int(training_line.shape[1]):
+100         for i in range(0,int(training_line.shape[1])-int(num_len)):
+101
+102
